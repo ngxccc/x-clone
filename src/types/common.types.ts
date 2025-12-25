5c0f30209d50e047ec6df0cdef8a27e47a56e6b3
shin-tran <ngoc.shintrann@gmail.com>
2025-12-26 00:01:03 +0700

    feat: add tokenType and user to express Resquest

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -1,9 +1,19 @@
+import { TokenType } from "@/constants/enums.js";
+import { Types } from "mongoose";
+
 export interface MongoError extends Error {
   code?: number;
   keyValue?: Record<string, unknown>;
 }
 
 export interface TokenPayload {
-  userId: string;
-  role: number;
+  userId: Types.ObjectId;
+  tokenType: TokenType;
+}
+
+// Declaration Merging (Gộp định nghĩa)
+declare module "express" {
+  interface Request {
+    user?: TokenPayload;
+  }
 }
