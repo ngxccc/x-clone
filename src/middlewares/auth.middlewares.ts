489ce3aca362436dc53dc0894041c979f07e9ff1
shin-tran <ngoc.shintrann@gmail.com>
2025-12-26 00:05:45 +0700

    feat: implement accessTokenValidator middleware for Bearer token validation

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -1,17 +1,29 @@
+import { verifyAccessToken } from "@/utils/jwt.js";
 import { NextFunction, Request, Response } from "express";
 
-export const loginValidator = (
+export const accessTokenValidator = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const { email, password } = req.body;
+  const authHeader = req.headers.authorization;
 
-  if (!email || !password) {
-    return res.status(400).json({
-      error: "Email và password không được để trống",
-    });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Yêu cầu Access Token (Bearer Token)" });
   }
 
-  next();
+  const token = authHeader.split(" ")[1]!;
+
+  try {
+    const decoded = verifyAccessToken(token);
+
+    // Gán thông tin user vào req để dùng ở Controller sau
+    req.user = decoded;
+
+    next();
+  } catch {
+    return res.status(401).json({ message: "Token hết hạn hoặc không hợp lệ" });
+  }
 };
