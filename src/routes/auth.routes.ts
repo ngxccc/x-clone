46289c6aaa3f31cc9381e30e9916aecba8a9d0b6
shin-tran <ngoc.shintrann@gmail.com>
2025-12-26 00:06:46 +0700

    feat: replace loginValidator with validate middleware for login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,14 +2,13 @@ import {
   loginController,
   registerController,
 } from "@/controllers/auth.controllers.js";
-import { loginValidator } from "@/middlewares/auth.middlewares.js";
 import { validate } from "@/middlewares/validate.middleware.js";
-import { RegisterReqBody } from "@/schemas/auth.schemas.js";
+import { LoginReqBody, RegisterReqBody } from "@/schemas/auth.schemas.js";
 import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post("/login", loginValidator, loginController);
+authRouter.post("/login", validate(LoginReqBody), loginController);
 authRouter.post("/register", validate(RegisterReqBody), registerController);
 
 export default authRouter;
