1a38c074465efc3a85950776c49bf6b2c48b3b6d
shin-tran <ngoc.shintrann@gmail.com>
2025-12-25 09:09:01 +0700

    feat: update res for standard response struct

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -7,11 +7,12 @@ export const loginController = (req: Request, res: Response) => {
 
   if (email === "ngxc@gmail.com" && password === "123123") {
     return res.status(200).json({
-      message: "Login success!",
+      message: "Đăng nhập thành công!",
     });
   }
 
   return res.status(401).json({
+    message: "Sai thông tin đăng nhập",
     error: "Email hoặc mật khẩu không chính xác",
   });
 };
