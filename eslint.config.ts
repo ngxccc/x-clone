ebc0bcd3f72f51a688125a5ea9f79c00a7805cd5
shin-tran <ngoc.shintrann@gmail.com>
2025-12-26 09:42:23 +0700

    feat: update ESLint configuration for improved Prettier integration and rule adjustments

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default defineConfig([
@@ -11,14 +13,19 @@ export default defineConfig([
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
   },
-  tseslint.configs.recommended,
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  // Tắt các rule xung đột với Prettier
+  eslintConfigPrettier,
   {
     plugins: {
       prettier: eslintPluginPrettier,
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
       "prettier/prettier": [
         "warn",
         {
@@ -28,6 +35,9 @@ export default defineConfig([
           endOfLine: "auto",
           useTabs: false,
           printWidth: 80,
+          trailingComma: "all",
+          singleQuote: false,
+          jsxSingleQuote: false,
         },
       ],
     },
